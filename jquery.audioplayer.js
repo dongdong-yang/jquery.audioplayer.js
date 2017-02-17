0558e8a2b9d710f1ad3f705d43935c45a45ac458
Dongdong Yang <hi@dongdong.ca>
2017-02-17 11:44:36 -0500

    Create jquery.audioplayer.js

---
+++ jquery.audioplayer.js
@@ -0,0 +1,8 @@
+/**
+ * @author Dongdong Yang <hi@dongdong.ca>
+ * Copyright (c) 2017 Dongdong Yang - released under MIT License
+ * https://github.com/stupidong/jquery.audioplayer.js
+ * Usage: var player= new AudioPlayer('http://example.org/sound')
+ * player.play();
+**/
+
