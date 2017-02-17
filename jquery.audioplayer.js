8aeec3019d757621566df2952821612e9aaf346e
Dongdong Yang <hi@dongdong.ca>
2017-02-17 13:39:40 -0500

    Update jquery.audioplayer.js

--- jquery.audioplayer.js
+++ jquery.audioplayer.js
@@ -6,3 +6,48 @@
  * player.play();
 **/
 
+ //  $.browser.webkit is removed after jquery 1.9
+ var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+
+ var AudioPlayer = function (path) {
+
+        var url = path;
+        var audio;
+
+        if (isSafari) {
+
+            audio = new Audio(url);
+
+            var startAudio = function () {
+                audio.play();
+                
+                document.removeEventListener("touchstart", startAudio, false);
+            }
+
+            var pauseAudio = function () {
+                audio.pause();
+                audio.removeEventListener("play", pauseAudio, false);
+            }
+            // Start playing audio when the user clicks anywhere on the page,
+            // to force Mobile Safari to load the audio.
+            document.addEventListener("touchstart", startAudio, false);
+            audio.addEventListener("play", pauseAudio, false);
+        }
+
+        this.play = function () {
+
+            if (audio) {
+                audio.play();
+            } else {
+
+                $('.audio-player').remove();
+
+                $(
+                   '<audio class="audio-player" autoplay="autoplay" style="display:none;">'
+                     + '<source src="' + url + '" />'
+                     + '<embed src="' + url + '" hidden autostart="true" loop="false"/>'
+                     + '</audio>'
+                 ).appendTo('body');
+            }
+        }
+    }
